0b706f76dc2893e8755df6a94ab92a2cbcbcd627
Irfan <assidiq.irfan@gmail.com>
2019-05-11 08:26:39 +0700

    add some components

---
+++ app/src/main/java/com/irfan/sampling/one2onechatapps/model/Friend.kt
@@ -0,0 +1,11 @@
+package com.irfan.sampling.one2onechatapps.model
+
+import com.irfan.sampling.one2onechatapps.data.ParseFirebaseData
+
+import java.io.Serializable
+
+/**
+ *   created by Irfan Assidiq on 2019-05-11
+ *   email : assidiq.irfan@gmail.com
+ **/
+class Friend(val id: String, val name: String, val photo: String) : Serializable
---
+++ app/src/main/java/com/irfan/sampling/one2onechatapps/utilities/Const.kt
@@ -0,0 +1,36 @@
+package com.irfan.sampling.one2onechatapps.utilities
+
+
+/**
+ *   created by Irfan Assidiq on 2019-05-11
+ *   email : assidiq.irfan@gmail.com
+ **/
+public class Const {
+
+    companion object {
+
+        const val NODE_TEXT = "text"
+        const val NODE_TIMESTAMP = "timestamp"
+        const val NODE_RECEIVER_ID = "receiverid"
+        const val NODE_RECEIVER_NAME = "receivername"
+        const val NODE_RECEIVER_PHOTO = "receiverphoto"
+        const val NODE_SENDER_ID = "senderid"
+        const val NODE_SENDER_NAME = "sendername"
+        const val NODE_SENDER_PHOTO = "senderphoto"
+        const val NODE_IS_READ = "isread"
+
+        //User nodes
+        const val NODE_ID = "id"
+        const val NODE_NAME = "name"
+        const val NODE_PHOTO = "photo"
+
+        const val LOG_TAG = "fchat"
+
+        const val MESSAGE_CHILD = "messages"
+
+        const val PREF_MY_ID = "myid"
+        const val PREF_MY_NAME = "myname"
+        const val PREF_MY_DP = "mydp"
+    }
+    //Chat nodes
+}
